5e740ccb35c091d126f093585d54608c5d82e1c5
Skipsy <skipsy10@gmail.com>
2026-03-31 02:16:25 +1100

    Bar Grahics working
    At the moment on the bar manager you can set how long the turns will be and how much space a status will take up in seconds it's refered to as crit for now but it's about getting a satus on the bar

--- Assets/Scripts/BarManager.cs
+++ Assets/Scripts/BarManager.cs
@@ -1,50 +1,97 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class BarManager : MonoBehaviour
 {
-    public float barTimer;
+    public float turnLength;
+    private float _barTimer;
     private bool _turnStarted = false;
+    public Image barImage;
+    public RectTransform barRectTransform;
+    public RectTransform barCritTransform;
+    public Image barCritImage;
 
-    public Image bar;
+    public float CritLength;
+    private bool _CritIsActive = false;
+    private float _CritTime;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
        TurnTimer();
+       statusUpdate();
+       
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+           if (_turnStarted == false)
+           {
+               StartTurn();
+           }
+       }
+       if (Input.GetKeyDown(KeyCode.Space)) 
+       {
+            CritBarSetup(Random.Range(2f, turnLength -1f));
+       }
     }
-
     public void StartTurn()
     {
         _turnStarted = true;
-        barTimer = 10f;
+        _barTimer = turnLength;
         // can play cards is true
     }
-
-    public void TurnTimer() 
+    public void TurnTimer() // Called on update
     {
         if (_turnStarted)
         {
-            barTimer -= Time.deltaTime;
-            Debug.Log(barTimer);
-            bar.fillAmount = barTimer/10f;
+            _barTimer -= Time.deltaTime;
+            barImage.fillAmount = BarPercentCalc(_barTimer, turnLength);
         }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if (_turnStarted == false)
-            {
-                StartTurn();
-            }
-        }
-        if (barTimer < 0)
+        
+        if (_barTimer < 0)
         {
             _turnStarted = false;
         }
+    }
+    public float BarPercentCalc(float current, float maxBar)
+    {
+        return current / maxBar;
+    }
+
+    public void CritBarSetup(float critplacement)
+    {
+        _CritIsActive = true;
+        _CritTime = critplacement;
+        barCritTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, SecondsToBarPercent(CritLength));
+        //barCritTransform.rect.width = SecondsToBarPercent(CritLength);
+        
+        Debug.Log(critplacement);
+        barCritTransform.position = barFinderX(barRectTransform,barCritTransform.rect.width) + FindBarPercent(barRectTransform,critplacement);
+        
+    }
+    public Vector3 barFinderX(RectTransform rectTransform, float statusBarLength)
+    {
+        return rectTransform.position + new Vector3(-rectTransform.rect.width / 2, 0, 0) -
+               new Vector3(statusBarLength/2, 0, 0);
+    }
+    public Vector3 FindBarPercent(RectTransform rectTransform, float critplacement)
+    {
+        return new Vector3(critplacement / turnLength * barRectTransform.rect.width, 0f, 0f);
+    }
+
+    public float SecondsToBarPercent(float seconds )
+    {
+        float percent = seconds / turnLength *100f;
+        return (percent/100f)*barRectTransform.rect.width;
+    }
+    public void statusUpdate()
+    {
+        if (_CritIsActive == true)
+        {
+            if (_CritTime >= _barTimer)
+            {
+               // barCritImage.fillAmount = BarPercentCalc()
+            }
+        }
+        
     }
 }
