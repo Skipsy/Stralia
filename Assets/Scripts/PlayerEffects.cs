3e377b8064ceca82aa51c0d2b3810162410bc139
bryce <brycejerickson@hotmail.com>
2026-05-12 19:41:05 +1000

    started player effects scriopt, got distracted by nora, currently unfinished

---
+++ Assets/Scripts/PlayerEffects.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerEffects : MonoBehaviour
+{
+
+    public enum Effects { 
+        normal = 0,
+        poisoned = 1, 
+        bleeding = 2, 
+        stunned = 4,
+        frozen = 8 }
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+}
+}
