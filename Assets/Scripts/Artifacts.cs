599fd936ad161704f22e1e0d18bda43e4f47ad3a
bryce <brycejerickson@hotmail.com>
2026-05-19 21:14:51 +1000

    Artifacts Script
    Artifacts added and some effects listed

---
+++ Assets/Scripts/Artifacts.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Artifacts", menuName = "Artifacts")]
+public class Artifacts : ScriptableObject
+{
+    public string name;
+    public string description;
+
+    public Sprite art;
+
+    public int artifactamountcount;
+
+
+
+}
--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemies")]
 public class Enemies : ScriptableObject
 {
 
